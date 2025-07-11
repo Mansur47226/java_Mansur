1b12ca13a9083be48f1490ce190e3a3bf64cd285
Mansur <mdadashev13@gmail.com>
2025-07-11 13:57:57 +0400

    String Class work
    Mansur String

--- Main.java
+++ Main.java
@@ -1,54 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        User u = new User();
+        String str1 = new String("добро пожаловать");
+        String str2 = new String("спасибо");
 
-        u.setUsername("Mansur5242");
-        u.setPassword("123");
-        u.setEmail("mdadashev13@gmail.com");
-        u.setBirthDate("2013-08-25");
-
-        System.out.println("Register");
-        System.out.println("username: " + u.getUsername());
-        System.out.println("password: " + u.getPassword());
-        System.out.println("email: " + u.getEmail());
-        System.out.println("date: " + u.getBirthDate());
-    }
-}
-class User {
-    private String username;
-    private String password;
-    private String email;
-    private String date;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.date = birthDate;
-    }
-
-    public String getBirthDate() {
-        return date;
+        System.out.println(str1.length());
+        System.out.println(str2.charAt(0));
+        System.out.println(str1.substring(str1.length() - 4));
+        if (str1.equals(str2)) {
+            System.out.println("строки одинаковые");
+        } else {
+            System.out.println("строки разные");
+        }
     }
 }
