837a0a701b1bf7248d1051452f4dd00ed137e12e
Mansur <mdadashev13@gmail.com>
2025-09-05 20:56:21 +0400

    new example
    mansur

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = new String("добро пожаловать");
-        String str2 = new String("спасибо");
+        Outer outer = new Outer();
+        outer.test();
+    }
+}
 
-        System.out.println(str1.length());
-        System.out.println(str2.charAt(0));
-        System.out.println(str1.substring(str1.length() - 4));
-        if (str1.equals(str2)) {
-            System.out.println("строки одинаковые");
-        } else {
-            System.out.println("строки разные");
+class Outer {
+    int outer_x = 100;
+    int outer_x_2 = 200;
+    int outer_x_3 = 3;
+
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+
+    class Inner {
+        void display() {
+            System.out.println((outer_x + outer_x_2) / outer_x_3);
         }
     }
 }
