6206c23aab8e6b4e1a1ebd1c97705a894ef5a3fc
Mansur <mdadashev13@gmail.com>
2025-09-14 13:37:55 +0400

    FIle
    Classwork

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.test();
-    }
-}
+        File myfile = new File("/Users/samirdadashov/Desktop/newfile");
 
-class Outer {
-    int outer_x = 100;
-    int outer_x_2 = 200;
-    int outer_x_3 = 3;
+        System.out.println(myfile.length());
+        System.out.println(myfile.getName());
 
-    void test() {
-        Inner inner = new Inner();
-        inner.display();
-    }
 
-    class Inner {
-        void display() {
-            System.out.println((outer_x + outer_x_2) / outer_x_3);
-        }
     }
 }
